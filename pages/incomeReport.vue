2e0d9e2dcee99059a3bae6561342d489561bb756
汪世傑 <louise@mosji.co>
2025-03-01 16:45:40 +0800

    b

--- pages/incomeReport.vue
+++ pages/incomeReport.vue
@@ -8,42 +8,63 @@
       <!-- <div class="bbb">b</div> -->
     </div>
     <div class="bbb grow w-full">
-      c
-
       <input type="file" @change="handleFiles" />
+      <div class="overflow-x-auto">
+        <table class="table table-xs">
+          <thead>
+            <tr>
+              <th></th>
+              <th>日期</th>
+              <th>早餐金額</th>
+              <th>早餐類型</th>
+              <th>午餐金額</th>
+              <th>午餐類型</th>
+              <th>晚餐金額</th>
+              <th>晚餐類型</th>
+              <th>額外花費</th>
+              <th>花費類型</th>
+              <th>當天總花費</th>
+            </tr>
+          </thead>
+          <tbody v-if="content">
+            <tr v-for="(data, index) in content.data">
+              <th>{{ index }}</th>
+              <td>{{ content.data[0 + 8][index] }}</td>
+              <td>{{ content.data[1 + 8][index] }}</td>
+              <input type="text" />
+              <td>{{ content.data[2 + 8][index] }}</td>
+              <input type="text" />
+              <td>{{ content.data[3 + 8][index] }}</td>
+              <input type="text" />
+              <td>{{ content.data[4 + 8][index] }}</td>
+              <input type="text" />
+              <td>{{ content.data[6 + 8][index] }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import Papa from "papaparse";
-const content = undefined;
+const content = ref(undefined);
+const dayIndex = ref(0);
+const monthIndex = ref(0);
+const rowSpacing = ref(8);
 
 const handleFiles = (element) => {
   Papa.parse(element.target.files[0], {
     complete: function (results) {
       console.log(results);
+      content.value = results;
     },
-    header: true, // keyed by field name
+    // header: true, // keyed by field name
   });
-};
-
-const handleConvert = (csvString) => {
-  var results = Papa.parse(csvString, config);
-  console.log(results);
-};
-
-const getProduct = async (id) => {
-  const { data, pending, error } = await useFetch(
-    `https://fakestoreapi.com/products/${id}`
-  );
-
-  console.log("after this" + id);
-  console.log(data.value);
 };
 
 onMounted(async () => {
   await nextTick();
-  await getProduct(2);
 });
 </script>
