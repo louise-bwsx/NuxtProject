345ae2e0cec428a38080d566829702bd24eb149d
汪世傑 <louise@mosji.co>
2025-09-09 23:06:25 +0800

    feat: 顯示google頭像

--- components/common/GoogleLoginButton.vue
+++ components/common/GoogleLoginButton.vue
@@ -1,11 +1,20 @@
 <template>
-  <button :disabled="!isReady" @click="login()">
-    <img src="@/assets/icons/account_40_40_white.svg" class="w-[40px] h-[40px] bg-black rounded-full ooo">
+  <button :disabled="!isReady" @click="login()" class="flex space-x-[8px] items-center">
+    <img v-if="isLogin" :src="picture" class="w-[40px] h-[40px] bg-black rounded-full">
+    <img v-else src="@/assets/icons/account_40_40_white.svg" class="w-[40px] h-[40px] bg-black rounded-full">
   </button>
 </template>
 
 <script setup>
 import { useCodeClient } from '#imports';
+import { useAuthStore } from '#imports';
+import { useToastStore } from '#imports';
+
+const authStore = useAuthStore()
+const toastStore = useToastStore()
+
+const isLogin = ref(false)
+const picture = ref("")
 
 const handleOnSuccess = (response) => {
   console.log(`response.code: ${response.code}`);
@@ -31,15 +40,21 @@ const verifyTokenWithBackend = async (code) => {
   try {
     const response = await useApiStore().post("/api/v1/auth/verify", body)
     if (response.code === 0) {
-      useAuthStore().setAccessToken(response.data.accessToken)
-      useAuthStore().setUserInfo(response.data.userInfo)
-      useToastStore().showToast("登入成功", "success")
+      authStore.setAccessToken(response.data.accessToken)
+      authStore.setUserInfo(response.data.userInfo)
+      toastStore.showToast("登入成功", "success")
     } else {
-      useToastStore().showToast(`登入失敗: ${response.message}`, "error")
+      toastStore.showToast(`登入失敗: ${response.message}`, "error")
     }
   } catch (error) {
     console.log(error.message)
-    useToastStore().showToast(`不明原因 登入失敗，請稍後再試: ${error.message}`, "error")
+    toastStore.showToast(`不明原因 登入失敗，請稍後再試: ${error.message}`, "error")
   }
 }
+
+onMounted(() => {
+  // 避免Hydration node mismatch 將結果存起來
+  isLogin.value = authStore.getUserInfo != undefined
+  picture.value = isLogin.value ? authStore.getUserInfo.picture : ''
+})
 </script>
