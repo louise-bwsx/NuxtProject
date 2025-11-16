477018d8000ce56e683df655aa84c2423ba3230f
汪世傑 <louise@mosji.co>
2025-11-16 20:28:42 +0800

    feat: 更新網站favicon 新增小遊戲

---
+++ pages/game.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="width:100%; height:100vh;">
+    <iframe src="/unity/index.html" style="width:100%; height:100%; border:none;"></iframe>
+  </div>
+</template>
+
+<!-- <template>
+  <canvas id="unity-canvas" style="width:100%; height:100vh;"></canvas>
+</template>
+
+<script setup>
+onMounted(() => {
+  const script = document.createElement("script")
+  script.src = "/unity/Build/TestWebBuild.loader.js"
+  script.onload = () => {
+    createUnityInstance(document.querySelector("#unity-canvas"), {
+      dataUrl: "/unity/Build/TestWebBuild.data",
+      frameworkUrl: "/unity/Build/TestWebBuild.framework.js",
+      codeUrl: "/unity/Build/TestWebBuild.wasm"
+    })
+  }
+  document.body.appendChild(script)
+})
+</script> -->
